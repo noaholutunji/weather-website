ae76394bac36e65b0072c0ac11eb87d347a0d949
Noah <osuolale49@gmail.com>
2019-07-31 14:11:23 +0100

    updated unit

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
- const url = 'https://api.darksky.net/forecast/1f2c4b1c7ec368d0db68a4b23c9ec18b/' + latitude + ',' + longitude
+ const url = 'https://api.darksky.net/forecast/1f2c4b1c7ec368d0db68a4b23c9ec18b/' + latitude + ',' + longitude + '?units=si'
 
  request({ url, json: true }, (error, { body }) => {
     if (error) {
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
     } else if (body.error) {
         callback('Unable to find location', undefined)
     } else {
-        callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. This high today is ' + body.daily.data[0].temperatureHigh + ' with a low of ' + body.daily.data[0].temperatureLow + '. There is a ' + body.currently.precipProbability + '% chance of rain.')
+        callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees celsius out. This high today is ' + body.daily.data[0].temperatureHigh + ' with a low of ' + body.daily.data[0].temperatureLow + '. There is a ' + body.currently.precipProbability + '% chance of rain.')
     }
 })
 }
